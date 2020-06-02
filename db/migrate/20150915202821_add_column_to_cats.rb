65fc82622bfc93f6aa07c1a0f05b478ad1ceaf6f
Franklyn Rodriguez <franklynr92@gmail.com>
2020-06-02 14:06:47 -0400

    Done.

--- db/migrate/20150915202821_add_column_to_cats.rb
+++ db/migrate/20150915202821_add_column_to_cats.rb
@@ -1,9 +1,10 @@
 class AddColumnToCats < ActiveRecord::Migration
-  def up
+  def change
     add_column :cats, :owner_id, :integer
   end
-
+=begin
   def down
     remove_column :cats, :owner_id
   end
+=end
 end
